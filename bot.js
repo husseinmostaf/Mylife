514147f66a3c9d46e1c0ba94644931ed4fcc520c
husseinmostaf <52549284+husseinmostaf@users.noreply.github.com>
2019-07-05 00:04:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`my life`,"http://twitch.tv/kiss")
+client.user.setGame(`kiss me`,"http://twitch.tv/kiss")
 client.user.setStatus("dnd")
 });
 
